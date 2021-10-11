255ce37a6b415bb210e1835c08495b4d3885c594
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-11 11:35:45 +0000

    adding seed.groovy file

--- seed.groovy
+++ seed.groovy
@@ -10,13 +10,13 @@ def count = (component.size() -1)
 for(int i in 0..count) {
 
 def j=component[i]
-pipelineJob('CI-Pipelines/${j}') {
+pipelineJob("CI-Pipelines/${j}") {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/${j}')
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/${j}")
           }
         }
         'branches' {
