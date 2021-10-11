f91d9d0713ff88b7c16f18e3c7a77cdb7f4ce4ac
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-11 07:39:21 +0000

    adding log.groovy file

---
+++ vars/Roboshop.groovy
@@ -0,0 +1,56 @@
+def call () {
+    pipeline {
+
+  agent {
+    node {
+      label 'NODEJS'
+    }
+  }
+
+  stages {
+
+    stage('Compiling the code') {
+      steps {
+        echo 'Code compilation done'
+      }
+    }
+
+    stage('Check Code Quality') {
+      steps {
+        echo 'Checking Code Quality'
+      }
+    }
+
+    stage('Lint Checks') {
+      steps {
+        echo 'Checking Lint Checks'
+      }
+    }
+
+    stage('Unit Tests') {
+      steps {
+        echo 'Unit tests'
+      }
+    }
+
+    stage('Prepare Artifact') {
+      steps {
+        sh '''
+          cd static
+          zip -r frontend.zip * 
+        '''
+      }
+    }
+
+    stage('Publish Artifacts') {
+      steps {
+        echo 'Publish Artifacts'
+      }
+    }
+
+  }
+
+}
+
+
+}
