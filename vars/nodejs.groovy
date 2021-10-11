1ee1d143bd271edeef635e7610736080aa678797
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-11 10:41:25 +0000

    adding log.groovy file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -6,12 +6,14 @@ def call (String COMPONENT) {
       label "NODEJS"
     }
   }
-
+  environment {
+      Sonar_Token = credentials('Sonar_Token')
+  }
   stages {
 
     stage('Check Code Quality') {
-      steps {
-        echo 'Checking Code Quality'
+      steps { 
+        sh """ sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.15.90:9000 -Dsonar.login=${Sonar_Token} """
       }
     }
 
