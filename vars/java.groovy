2910631af7fdf24238f6220187325691c091fd06
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-12 05:37:36 +0000

    adding a file

---
+++ vars/java.groovy
@@ -0,0 +1,62 @@
+def call (String COMPONENT) {
+    pipeline {
+
+  agent {
+    node {
+      label "NODEJS"
+    }
+  }
+  environment {
+      Sonar_Token = credentials('Sonar_Token')
+  }
+  stages {
+    stage('CSubmitting for code Compilation') {
+        steps {
+            sh 'mvn target/.'
+        }
+    }
+
+    stage('Checking Code Quality') {
+      steps { 
+        sh """ sonar-scanner -Dsonar.projectKey=${COMPONENT} -Dsonar.sources=. -Dsonar.host.url=http://172.31.25.74:9000 -Dsonar.login=${Sonar_Token} """
+      }
+    }
+
+    stage('Checking for Lints ') {
+      steps {
+        echo 'Checking Lint Checks'
+      }
+    }
+
+    stage('Unit Tests') {
+      steps {
+        echo 'Unit tests'
+      }
+    }
+
+    stage('Preparing Artifact') {
+      steps {
+        sh """
+          cd static
+          zip -r ${COMPONENT}.zip * 
+        """
+      }
+    }
+
+    stage('Publishing Artifacts') {
+      steps {
+        echo 'Publish Artifacts'
+      }
+    }
+
+  }
+
+  post {
+    always {
+      cleanWs()
+    }
+  }
+}
+
+
+}
