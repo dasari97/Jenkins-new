6b08f6f987b703c968386e803d1f63845c337d9d
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-13 08:40:48 +0000

    modified files

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -47,7 +47,7 @@ def call (String COMPONENT) {
         sh """
           cd static
           VERSION=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
-          zip -r ${COMPONENT}-\${VERSION}.zip *
+          zip -r ../${COMPONENT}-\${VERSION}.zip *
         """
       }
     }
