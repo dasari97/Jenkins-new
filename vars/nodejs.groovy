fc8ae3824f012f263d7d47817c582726adf774a1
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-13 07:14:41 +0000

    modified files

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -50,7 +50,7 @@ def call (String COMPONENT) {
         sh """
           npm install
           VERSION=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
-          zip -r ${COMPONENT}-\${VERSION}.zip node_module server.js
+          zip -r ${COMPONENT}-\${VERSION}.zip node_modules server.js
         """
       }
     }
