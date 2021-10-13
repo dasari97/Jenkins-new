349d54086346117f7f064e229ed43bb4c3ff99c8
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-13 08:45:30 +0000

    modified files

--- vars/nginx.groovy
+++ vars/nginx.groovy
@@ -57,7 +57,7 @@ def call (String COMPONENT) {
       steps {
         sh """ 
         VERSION=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
-        curl -v -u ${NEXUS} --upload-file ${COMPONENT}-\${VERSION}.zip http://172.31.4.112:8081/repository/${COMPONENT}/${COMPONENT}-\${VERSION}.zip 
+        curl -f -v -u ${NEXUS} --upload-file ${COMPONENT}-\${VERSION}.zip http://172.31.4.112:8081/repository/${COMPONENT}/${COMPONENT}-\${VERSION}.zip 
         """
       }
     }
