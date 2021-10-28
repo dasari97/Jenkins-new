db7319c95580b2889f6ca6a5e6856920d4c8d71c
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-28 08:15:17 +0000

    adding new file

--- seed.groovy
+++ seed.groovy
@@ -29,9 +29,72 @@ pipelineJob("CI-Pipelines/${j}") {
           }
         }
       }
-      'scriptPath'('Jenkinsfile')
+      'scriptPath'('jenkinsfile')
       'lightweight'(true)
     }
   }
  }
 }
+
+
+folder('MI-Terraform') {
+  displayName('MI-Terraform')
+  description('MI-Terraform')
+}
+
+
+pipelineJob("MI-Terraform/VPC") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('vpc/jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
+ 
+ folder('MI-Terraform') {
+  displayName('MI-Terraform')
+  description('MI-Terraform')
+}
+
+
+pipelineJob("MI-Terraform/DB") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('databases/Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
