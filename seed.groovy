3e521ce89c028344505be671483331503c27d757
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-28 08:53:04 +0000

    adding new file

--- seed.groovy
+++ seed.groovy
@@ -37,3 +37,64 @@ pipelineJob("CI-Pipelines/${j}") {
 }
 
 
+folder('MI-Terraform') {
+  displayName('MI-Terraform')
+  description('MI-Terraform')
+}
+
+
+pipelineJob("MI-Terraform/VPC") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('vpc/jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
+ 
+ folder('MI-Terraform') {
+  displayName('MI-Terraform')
+  description('MI-Terraform')
+}
+
+
+pipelineJob("MI-Terraform/DB") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('databases/jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
