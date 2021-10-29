9b04ba75c29366af970bcad4ada43060952f923b
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-29 15:34:42 +0000

    adding new file

--- seed.groovy
+++ seed.groovy
@@ -73,3 +73,34 @@ pipelineJob("Application_MI/${j}") {
   }
  }
 }
+
+folder('MI') {
+  displayName('MI')
+  description('MI')
+}
+
+pipelineJob("MI/ALB") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('ALB/jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
+}
