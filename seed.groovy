8269e745a8b7b7f7b246a0a1afc7b63f7b0f76fe
dasari97 <dasarisaikrishna97@gmail.com>
2021-10-29 14:13:46 +0000

    adding new file

--- seed.groovy
+++ seed.groovy
@@ -36,64 +36,7 @@ pipelineJob("CI-Pipelines/${j}") {
  }
 }
 
-folder('Mutable-Infra') {
-  displayName('Mutable-Infra')
-  description('Mutable-Infra')
-}
-pipelineJob("Mutable-Infra/VPC") {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
-            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
-            }
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('VPC/jenkinsfile')
-      'lightweight'(true)
-    }
-  }
- }
- 
- folder('Mutable-Infra') {
-  displayName('Mutable-Infra')
-  description('Mutable-Infra')
-}
-pipelineJob("Mutable-Infra/DB") {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/terraform-mutable")
-            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-              'name'('*/tags/*')
-            }
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Databases/jenkinsfile')
-      'lightweight'(true)
-    }
-  }
- }
- 
+
  pipelineJob("Mutable-Infra/Destroy") {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
