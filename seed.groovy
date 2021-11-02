1112f8f70ab0033ea9cd055cf35dd7a6052f19ae
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-02 10:11:31 +0000

    adding new file

--- seed.groovy
+++ seed.groovy
@@ -36,3 +36,41 @@ pipelineJob("Application_MI/${j}") {
  }
 }
 
+folder('CI-Pipelines') {
+  displayName('CI-Pipelines')
+  description('CI-Pipelines')
+}
+
+def component = ["frontend", "catalogue", "cart", "shipping", "payment", "user"]
+
+def count = (component.size() -1)
+
+for(int i in 0..count) {
+
+def j=component[i]
+pipelineJob("CI-Pipelines/${j}") {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'("https://dasarisaikrishna97@dev.azure.com/dasarisaikrishna97/Roboshop/_git/${j}")
+            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+              'name'('*/tags/*')
+            }
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+ }
+}
+
