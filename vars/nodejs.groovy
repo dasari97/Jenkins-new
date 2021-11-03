a9644570df827c6f78e639541d5e8200ce014ded
dasari97 <dasarisaikrishna97@gmail.com>
2021-11-03 09:25:01 +0000

    adding new file

--- vars/nodejs.groovy
+++ vars/nodejs.groovy
@@ -61,7 +61,7 @@ def call (String COMPONENT) {
       steps {
         sh """ 
         VERSION=`echo ${GIT_BRANCH} | awk -F / '{print \$NF}'`
-        curl -f -v -u ${NEXUS} --upload-file ${COMPONENT}-\${VERSION}.zip http://172.31.11.102:8081/repository/${COMPONENT}/${COMPONENT}-\${VERSION}.zip 
+        curl -f -v -u ${NEXUS} --upload-file ${COMPONENT}-1.0.3 http://172.31.11.102:8081/repository/${COMPONENT}/${COMPONENT}-1.0.3
         """
       }
     }
